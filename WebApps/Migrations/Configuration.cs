05b3fa5cd64dc963896f3898d7541cbcc041c37e
Jannung Kusdiantoro <iceboltz@gmail.com>
2020-03-05 23:25:56 +0700

    update sitik

---
+++ WebApps/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApps.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApps.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "WebApps.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(WebApps.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
